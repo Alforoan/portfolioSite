d6e9e4e539f0bf53a02f69bf123c778c015e2dd9
Jay <jdragon2050@gmail.com>
2023-01-20 14:30:27 -0800

    add css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <Wrapper>
       <h1>Alforoan</h1>
-      <ul>
+      <ul className="page-link-list">
         <li>
           <a href="">Home</a>
         </li>
@@ -16,13 +16,35 @@ function Navbar() {
           <a href="">Projects</a>
         </li>
       </ul>
-      <button>
+      <button className="twitter-icon">
         <i class="fa-brands fa-twitter"></i>
       </button>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  width: 80vw;
+  max-width: 1100px;
+  justify-content: space-between;
+  margin: auto;
+  align-items: center;
+  align-content: center;
+  .page-link-list {
+    display: flex;
+    list-style: none;
+  }
+  .page-link-list a {
+    text-decoration: none;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+  .twitter-icon {
+    background: none;
+    border: none;
+    font-size: 2rem;
+  }
+`;
 
 export default Navbar;
